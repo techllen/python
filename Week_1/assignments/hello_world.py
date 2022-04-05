e9861b9a062e823f39146475a068e98339782b78
techllen <allenmwita21@gmail.com>
2022-04-05 12:17:18 -0400

    strings

---
+++ Week_1/assignments/hello_world.py
@@ -0,0 +1,36 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print("Hello",name)	# with a comma
+print("Hello"+ name)	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print("Hello",name,"!")	# with a comma
+print("Hello"+ str(name) + "!")	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}".format(fave_food1,fave_food2)) # with .format()
+print(f"I love to eat {fave_food1} and {fave_food2}") # with an f string
+
+#5.Bonus String manipulation
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+long_string = "I love coding dojo"
+
+print(a[1])
+print("love" in long_string)
+print(long_string[-6:-1])
+print(long_string.split("o"))
+
+print("I love\"\n moonfall\""+ str(2))
+
+for x in long_string:
+    print(x)
+
+
+
--- Week_1/assignments/test.py
+++ Week_1/assignments/test.py
@@ -1,14 +1,49 @@
-fruits = ["orange","pawpaw",2,2.5]
-myName = "allen"
-age = 29
-born = 1992
-print(f"{myName} {age} {born}")
-print(fruits)
-print("my name is" + str(age) )
+# fruits = ["orange","pawpaw",2,2.5]
+# myName = "allen"
+# age = 29
+# born = 1992
+# print(f"{myName} {age} {born}")
+# print(fruits)
+# print("my name is" + str(age) )
 
-weight = 10.9
-print(int(weight))
+# weight = 10.9
+# print(int(weight))
 
-numbers = [1,2,3,4,5,6,7]
+# numbers = [78,22,11,1,2,3,4,5,6,7]
 
-print(numbers[1:])
+# print(numbers[1:])
+# print(enumerate(numbers))
+# print(min(numbers))
+# print(sorted(numbers))
+
+
+# x = "First string"
+# y = "Second string"
+
+# print(x +" "+ y)
+# print(x,y)
+# print(f"{x} {y}")
+
+# x = [3,8,4]
+
+# y = (-1,True,15)
+# x.append("numbers")
+# x.insert(2,99)
+# x.remove(99)
+# x.pop(3)
+# x.reverse
+# print(x)
+# print(y)
+
+# someDictionary = {
+#     1:2,
+#     2:200,
+#     "cars": ["honda","toyota"]
+# }
+
+# print(someDictionary.get("cars"))
+
+def list_cars():
+    return ("camaro","ford")
+
+print(list_cars())
