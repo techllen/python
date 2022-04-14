b1f584e03811c1c67f6a162dd50fc7511c74e9d9
techllen <allenmwita21@gmail.com>
2022-04-13 21:48:36 -0400

    OOP

--- Week_2/input/lecture.py.py
+++ Week_2/input/lecture.py.py
@@ -12,19 +12,55 @@
 # print(carrier1.name)
 # print(carrier2.name)
 
-class Rename_Overseer:
-    all_carriers = []
-    overseer = "FAA"
+# class Rename_Overseer:
+#     all_carriers = []
+#     overseer = "FAA"
     
-    @classmethod
-    def rename_overseer(cls,new_name):
-        cls.overseer = new_name
+#     @classmethod
+#     def rename_overseer(cls,new_name):
+#         cls.overseer = new_name
         
-    @staticmethod
-    def has_enough_workers(current_workforce,needed_workers):
-        if(current_workforce >= needed_workers):
-            print("Enough workers as is")
-            return True
-        elif(current_workforce <= needed_workers):
-            print("Hire More Workers")
-            return False
+#     @staticmethod
+#     def has_enough_workers(current_workforce,needed_workers):
+#         if(current_workforce >= needed_workers):
+#             print("Enough workers as is")
+#             return True
+#         elif(current_workforce <= needed_workers):
+#             print("Hire More Workers")
+#             return False
+
+class Carrier: # Define your class here
+    all_carriers = [] # Class variable: list that will hold your carriers
+    overseer = "Federal Aviation Administration" # Class variable
+
+    def __init__(self, year, name, city):
+        self.year = year
+        self.name = name
+        self.city = city
+        self.flights = [] # List of flights
+        self.total_workers = 0 # Workforce for this carrier
+        Carrier.all_carriers.append(self) # Add this carrier
+
+class Flight:
+    def __init__(self, number, starting_city, ending_city):
+        self.number = number
+        self.starting_city = starting_city
+        self.ending_city = ending_city
+
+
+cd_airlines = Carrier(2022, "Coding Dojo Airlines", "Dojo City")
+my_airline = Carrier(1903, "Wright Bros. Flights", "Kitty Hawk")
+flight_one = Flight(555, "Seattle", "Reno")
+flight_two = Flight(100, "Phoenix", "London")
+
+cd_airlines.flights.append(flight_one)
+cd_airlines.flights.append(flight_two)
+
+my_airline.flights.append(flight_one)
+my_airline.flights.append(flight_two)
+
+print(str(cd_airlines.flights[0]))
+
+
+
+
--- Week_2/lecture/directors_movies_OOP.py
+++ Week_2/lecture/directors_movies_OOP.py
@@ -5,6 +5,7 @@ class Director:
         self.first_name = first_name
         self.last_name = last_name
         self.birthday = birthday
+        self.movie = []
         Director.all_directors.append(self)
         
     def start_scene(self):
@@ -18,6 +19,10 @@ class Director:
     @classmethod
     def rename_guild(cls,new_guild_name):
         cls.guild = new_guild_name
+        
+    def add_movie(self,new_movie):
+        self.movie.append(new_movie)
+        return self
     
 class Movie:
     rater = "MPAA"
@@ -26,6 +31,7 @@ class Movie:
         self.genre = genre
         self.release_date = release_date
         self.box_office = box_office
+        self.director = []
         
     @staticmethod
     def made_enough_money(box_office,cost):
@@ -33,6 +39,10 @@ class Movie:
             print("This movie is a Bomb")
         elif(box_office < cost):
             print("At Least we broke even or made money")
+    
+    def add_director(self,new_director):
+        self.director.append(new_director)
+    
     
 spielberg = Director("steven","Spielberg","1946-12-18")
 et = Movie("ET","Sci-fi","1982",250000000)
@@ -50,4 +60,12 @@ for this_director in Director.all_directors:
     
 Director.rename_guild("Kilo Maka")
 print(f"After:{Director.guild}")
+
+Movie.made_enough_money(200,300)
+
+spielberg.add_movie("Unchartered")
+print (spielberg.movie[0])
+
+et.add_director("Jordan")
+print(et.director[0])
 
