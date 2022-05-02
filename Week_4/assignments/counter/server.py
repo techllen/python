dbb3816e166ddc667998150df3005f1e0cbfa3c9
techllen <allenmwita21@gmail.com>
2022-05-02 02:23:24 -0400

    flask

--- Week_4/assignments/counter/server.py
+++ Week_4/assignments/counter/server.py
@@ -6,46 +6,74 @@ app.secret_key = secrets.token_hex()
 
 # Have the root route render a template that displays the number of times the client has visited this site. Refresh the page several times to ensure the counter is working.
 # index route
+total = 0
 @app.route('/')
 def show_counter():
     # printing the key in hex format
-    print(app.secret_key)
+    # print(app.secret_key)
     # checking if we have a count in session and add one to it
     if 'count' in session:
         session['count'] += 1
     # with no count in session we will put it as one because its the first time
     else:
         session['count'] = 1    
-    return render_template('index.html',count = session['count'])
+        
+    #checking if cookies exists and add them to the total
+    if 'increment_by_two' in session:
+        total = int(session['count']) + int(session['increment_by_two'])
+    elif 'increments' in session:
+        total = int(session['count']) + int(session['increments'])
+
+    elif ('increment_by_two' in session) and ('increments' in session):
+        total = int(session['count']) + int(session['increment_by_two']) + int(session['increments'])
+        
+    else:
+        total = session['count']
+    session['total'] = total    
+    return render_template('index.html')
 
 # Add a "/destroy_session" route that clears the session and redirects to the root route. Test it.
 # clear route
 @app.route('/destroy_session')
 def destroy_session():
-    # getting rid of everything currently whats stored in session
+    # getting rid of everything currently stored in the session
     session.clear()
     return redirect('/')
 
 
 # NINJA BONUS: Add a +2 button underneath the counter and a new route that will increment the counter by 2
 # increment by 2 route
-@app.route('/increment_by_2')
-def increment_by_2():
-    session['count'] +=2
-    return render_template('index.html',count = session['count'])
+@app.route('/increment_by_two')
+def increment_by_two():
+    if 'increment_by_two' not in session:
+        session['increment_by_two'] = 2
+    else:
+        session['increment_by_two'] += 2 
+    return redirect('/') 
 
 # NINJA BONUS: Add a Reset button to reset the counter
 # reset counter
 @app.route('/reset_counter')
 def reset_counter():
-    session.pop('count',None)
+    session.pop('count')
+    if 'increments' in session:
+        session.pop('increments')
+    if 'increments_by_2' in session:
+        session.pop('increment_by_two')
+    if 'total' in session:
+        session.pop('total')
+    session.clear()
     return redirect('/')
 
 # SENSEI BONUS: Add a form that allows the user to specify the increment of the counter and have the counter increment accordingly
 # increment using form
 @app.route('/increment_form',methods = ['POST'])
 def increment_using_form():
-    session['count'] += int(request.form['increment'])        
+    increment = int(request.form['increment'])
+    if 'increments' not in session:
+        session['increments'] = increment
+    else:
+        session['increments'] += increment
     return redirect('/')
 
 # errors
@@ -58,6 +86,6 @@ def error(e):
 print(base64.urlsafe_b64decode('eyJjb3VudCI6Mn0==='))
 
 
-# Run the app
+# Run the app in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Week_4/assignments/ninja_gold/server.py
@@ -0,0 +1,79 @@
+from flask import Flask,render_template,redirect,request,session
+import secrets
+import random
+import datetime
+app = Flask(__name__)
+
+# global variables
+app.secret_key = secrets.token_hex()
+activities = []
+
+# index route
+@app.route('/')
+def home():
+    # tatal al golds before rendering
+    # gold_total = session['farm_gold_to_give'] + session['cave_gold_to_give'] + session['house_gold_to_give'] + session['casion_gold_to_give'] + session['casion_gold_to_take_away']
+    return render_template('index.html')
+
+# process money route
+@app.route('/process_money' , methods = ['POST'])
+def process_money():
+    if request.form['form_type'] == 'farm':
+        # generate random number if the form has been accessed
+        session['farm_gold_to_give'] = random.randint(10,20)
+        session['gold_total'] = + session['farm_gold_to_give']
+        time = datetime.datetime.now()
+        activity = ['win','Earned '+ str(session['farm_gold_to_give']) + ' golds from farm! ' +'('+ str((time))+')']
+        activities.append(activity)
+        
+    elif request.form['form_type'] == 'cave':
+        # generate random number if the form has been accessed
+        session['cave_gold_to_give'] = random.randint(5,10)
+        session['gold_total'] = session['cave_gold_to_give']
+        time = datetime.datetime.now()
+        activity = ['win','Earned '+ str(session['cave_gold_to_give']) + ' golds from cave! ' +'('+ str((time))+')']
+        activities.append(activity)
+        
+    elif request.form['form_type'] == 'house':
+        # generate random number if the form has been accessed
+        session['house_gold_to_give'] = random.randint(2,5)
+        session['gold_total'] = session['house_gold_to_give']
+        time = datetime.datetime.now()
+        activity = ['win','Earned '+ str(session['house_gold_to_give']) + ' golds from house! ' +'('+ str((time))+')']
+        activities.append(activity)
+    
+    elif request.form['form_type'] == 'casino':
+        # generate random number if the form has been accessed
+        session['casino_gold_to_give_and_take_away'] = random.randint(0,50)
+        # generating random events 1 for win and 2 for loss
+        random_event = random.randint(1,2)
+        # if a win event occur
+        if random_event == 1:
+            time = datetime.datetime.now()
+            activity = ['win','Earned '+ str(session['casino_gold_to_give_and_take_away']) + ' golds from casino! ' +'('+ str((time))+')']
+        # when a loss event occur
+        elif random_event == 2:
+            time = datetime.datetime.now()
+            activity = ['loss','Entered a casino and lost '+ str(session['casino_gold_to_give_and_take_away']) + ' .....Ouch..... ' +'('+ str((time))+')']
+        activities.append(activity) 
+        
+    session['activities'] = activities
+    # + session['cave_gold_to_give'] 
+    # + session['house_gold_to_give'] + session['casion_gold_to_give_and_take_away']        
+    return redirect('/')
+
+# reset
+@app.route('/reset')
+def restart_game():
+    activities.clear()
+    session.clear()
+    return render_template('index.html')
+
+# errorpage
+@app.errorhandler(404)
+def errors(e):
+    return render_template('errors.html')
+
+# debugging mode
+if __name__ == '__main__':
+    app.run(debug=True)
